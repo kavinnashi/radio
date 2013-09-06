914ac5cb5df9bb8adc02b2c0df1f3b8376f1979b
kavinnashi <kavinnashi@gmail.com>
2013-09-06 17:26:23 +0530

    Update radio.js

--- radio.js
+++ radio.js
@@ -3,6 +3,8 @@
 //contains another function change which is used for firing the action on radio button click.
 //group class name=inpit,
 //elemnet id=input1,input2
+//this is for jquery 1.4.2 
+// for higher version change the live() to on()
 
 (function($) {
 
